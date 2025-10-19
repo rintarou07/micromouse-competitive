ee4c59dd969f60e90ebb8f43758cfe4ef1be9606
unknown <rintarou07@github.com>
2025-10-20 03:59:34 +0700

    done scanning

--- Main.py
+++ Main.py
@@ -2,164 +2,232 @@ import API
 import sys
 from collections import deque 
 
-class Cell:
-    def __init__(self, distance):
-        self.visited = False
-        self.wall = {
-            "N": False,
-            "S": False,
-            "E": False,
-            "W": False,
-        }
-        self.distance = distance
 
-    def initWall(self, direction, side):
-        if direction == (0, 1):
-            if side == "front":
-                self.wall["N"] = True
-                return "N"
-            if side == "back":
-                self.wall["S"] = True
-                return "S"
-            if side == "left":
-                self.wall["W"] = True
-                return "W"
-            if side == "right":
-                self.wall["E"] = True
-                return "E"
-        if direction == (1, 0):
-            if side == "front":
-                self.wall["E"] = True
-                return "E"
-            if side == "back":
-                self.wall["W"] = True
-                return "W"
-            if side == "left":
-                self.wall["N"] = True
-                return "N"
-            if side == "right":
-                self.wall["S"] = True
-                return "S"
-        if direction == (0, -1):
-            if side == "front":
-                self.wall["S"] = True
-                return "S"
-            if side == "back":
-                self.wall["N"] = True
-                return "N"
-            if side == "left":
-                self.wall["E"] = True
-                return "E"
-            if side == "right":
-                self.wall["W"] = True
-                return "W"
-        if direction == (-1, 0):
-            if side == "front":
-                self.wall["W"] = True
-                return "W"
-            if side == "back":
-                self.wall["E"] = True
-                return "E"
-            if side == "left":
-                self.wall["S"] = True
-                return "S"
-            if side == "right":
-                self.wall["N"] = True
-                return "N"
+    
 
 
-goal = (7, 7)
-start = (0, 0)
-WIDTH = API.mazeWidth()
-HEIGHT = API.mazeHeight()
+WIDTH = 16#API.mazeWidth()
+HEIGHT =16 #API.mazeHeight()
+GOAL = (7,7)
 DIRECTIONS = {
-    "N": (0,1),
-    "S": (0,-1),
-    "W": (-1,0),
-    "E": (1,0)
+    "right": (1,0),
+    "left": (-1,0),
+    "front": (0,1),
+    "back": (0,-1)
 }
-maze = []
-for column in range(WIDTH):
-    mazeColumn = []
+distances = []
+walls = [] #wall[x][y][0->3] 0: north(phia bac), 1:east(phia dong), 2:south(phia nam), 3:west(phia tay)
+visited = []
+class Mouse:
+    def __init__(self):
+        self.currentx = 0
+        self.currenty = 0
+        self.direction = (0,1)
+    
+for column in range( WIDTH):
+    mazecolumn = []
+    wallcolumn = []
+    visitedcolumn = []
     for row in range(HEIGHT):
-        mazeColumn.append(Cell(abs(goal[0] - column) + abs(goal[1] - row)))
-    maze.append(mazeColumn)
+        mazecolumn.append(abs(GOAL[0]-column)+abs(GOAL[1]-row))
+        wallcolumn.append([False,False,False,False])
+        visitedcolumn.append(False)
+    distances.append(mazecolumn)
+    walls.append(wallcolumn)
+    visited.append(visitedcolumn)
+
+def getOpenNeighbors(x,y,walls):
+    neighborList = []
+    if y<HEIGHT-1 and not walls[x][y][0] and not walls[x][y+1][2]:
+        neighborList.append((x,y+1))
+    if x<WIDTH-1 and not walls[x][y][1] and not walls[x+1][y][3]:
+        neighborList.append((x+1,y))
+    if y>0 and not walls[x][y][2] and not walls[x][y-1][0]:
+        neighborList.append((x,y-1))
+    if x>0 and not walls[x][y][3] and not walls[x-1][y][1]:
+        neighborList.append((x-1,y))
+    return neighborList
+
+def fullTurnRight(mouse):
+    API.turnRight()
+    mouse.direction = (mouse.direction[0]*0 -mouse.direction[1]*(-1),mouse.direction[0]*(-1) + mouse.direction[1]*0)
+def fullTurnLeft(mouse):
+    API.turnLeft()
+    mouse.direction = (mouse.direction[0]*0 -mouse.direction[1]*1,mouse.direction[0]*1 + mouse.direction[1]*0)
+def fullMoveForward(mouse):
+    API.moveForward()
+    mouse.currentx += mouse.direction[0]
+    mouse.currenty += mouse.direction[1]
+def updateWall(walls,x,y,direction,side):
+    if side == "right":
+        if direction == (1,0):
+            walls[x][y][2] = True
+            if y > 0:
+                walls[x][y-1][0] = True
+        if direction == (-1,0):
+            walls[x][y][0] = True
+            if y < HEIGHT-1:
+                walls[x][y+1][2] = True
+        if direction == (0,1):
+            walls[x][y][1] = True
+            if x<WIDTH-1:
+                walls[x+1][y][3] = True
+        if direction == (0,-1):
+            walls[x][y][3] = True
+            if x > 0:
+                walls[x-1][y][1] = True
+    if side == "left":
+        if direction == (1,0):
+            walls[x][y][0] = True
+            if y < HEIGHT-1:
+                walls[x][y+1][2] = True
+        if direction == (-1,0):
+            walls[x][y][2] = True
+            if y > 0:
+                walls[x][y-1][0] = True
+        if direction == (0,1):
+            walls[x][y][3] = True
+            if x > 0:
+                walls[x-1][y][1] = True
+        if direction == (0,-1):
+            walls[x][y][1] = True
+            if x<WIDTH-1:
+                walls[x+1][y][3] = True
+
+    if side == "front":
+        if direction == (1,0):
+            walls[x][y][1] = True
+            if x<WIDTH-1:
+                walls[x+1][y][3] = True
+        if direction == (-1,0):
+            walls[x][y][3] = True
+            if x > 0:
+                walls[x-1][y][1] = True
+        if direction == (0,1):
+            walls[x][y][0] = True
+            if y < HEIGHT-1:
+                walls[x][y+1][2] = True
+        if direction == (0,-1):
+            walls[x][y][2] = True
+            if y > 0:
+                walls[x][y-1][0] = True
+def getMinDistance(x,y,walls):
+    minDistance = 1000
+    openNeighbors = getOpenNeighbors(x, y,walls)
+    for coordinate in openNeighbors:
+        if minDistance>distances[coordinate[0]][coordinate[1]]:
+            minDistance = distances[coordinate[0]][coordinate[1]]
+    return minDistance
+def move(mouse,distances,walls):
+    minDistance = getMinDistance(mouse.currentx,mouse.currenty,walls)
+    minCoordinate = ()
+    openNeighbors = getOpenNeighbors(mouse.currentx, mouse.currenty,walls)
+    # if (mouse.currentx, mouse.currenty+1) in openNeighbors and distances[mouse.currentx][mouse.currenty+1] == minDistance:
+    #     return (mouse.currentx, mouse.currenty+1)
+
+    for coordinate in openNeighbors:
+        if distances[coordinate[0]][coordinate[1]] == minDistance:
+            minCoordinate = coordinate
+            break
+    if mouse.currentx == 1 and mouse.currenty == 3:
+        log(minCoordinate)
+    currentDirection = mouse.direction
+    if mouse.currentx + currentDirection[0] == minCoordinate[0] and mouse.currenty +currentDirection[1] == minCoordinate[1]:
+        fullMoveForward(mouse)
+        return
+    currentDirection = (mouse.direction[0]*0 -mouse.direction[1]*(-1),mouse.direction[0]*(-1) + mouse.direction[1]*0)
+    if mouse.currentx + currentDirection[0] == minCoordinate[0] and mouse.currenty +currentDirection[1] == minCoordinate[1]:
+        fullTurnRight(mouse)
+        fullMoveForward(mouse)
+        return
+    currentDirection = (mouse.direction[0]*0 -mouse.direction[1]*1,mouse.direction[0]*1 + mouse.direction[1]*0)
+    if mouse.currentx + currentDirection[0] == minCoordinate[0] and mouse.currenty +currentDirection[1] == minCoordinate[1]:
+        fullTurnLeft(mouse)
+        fullMoveForward(mouse)
+        return
+    else:
+        fullTurnLeft(mouse)
+        fullTurnLeft(mouse)
+        fullMoveForward(mouse)
+        return
 
 
+def floodFill(currentx,currenty,distances,walls):
+    q = deque()
+    q.appendleft((currentx,currenty))
+    while q:
+        (x,y) = q.popleft()
+        if (x,y) == GOAL:
+            continue
+        minDistance = getMinDistance(x,y,walls)
+        if distances[x][y] - 1 != minDistance:
+            distances[x][y] = minDistance + 1
+            if x>0 and distances[x-1][y] != 0:
+                q.appendleft((x-1,y))
+            if y > 0 and distances[x][y-1] != 0:
+                q.appendleft((x,y-1))
+            if y < HEIGHT-1 and distances[x][y+1] != 0:
+                q.appendleft((x,y+1))
+            if x < WIDTH-1 and distances[x+1][y] != 0:
+                q.appendleft((x+1,y))
+    return
+def NewfloodFill(distances, walls, visited):
+    q = deque()
+    found_unvisited = False
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            if not visited[x][y]:
+                distances[x][y] = 0
+                q.append((x,y)) 
+                found_unvisited = True
+            else:
+                distances[x][y] = 255 
+
+    if not found_unvisited:
+        return False
+    while q:
+        (x,y) = q.popleft()
+        current_dist = distances[x][y]
+        
+        for (nx, ny) in getOpenNeighbors(x, y, walls):
+            if visited[nx][ny] and distances[nx][ny] == 255: 
+                distances[nx][ny] = current_dist + 1
+                q.append((nx, ny))
+                
+    return True 
+def displayDistances(distances):
+    for y in range(HEIGHT):
+        for x in range (WIDTH):
+            API.setText(x,y,distances[x][y])
 def log(string):
     sys.stderr.write("{}\n".format(string))
     sys.stderr.flush()
-
-
-def rotateLeft(direction):
-    (x, y) = direction
-    newx = x * 0 - y * 1
-    newy = x + y * 0
-    return (newx, newy)
-
-
-def rotateRight(direction):
-    (x, y) = direction
-    newx = x * 0 - y * (-1)
-    newy = x * (-1) + y * 0
-    return (newx, newy)
-
-def initWall(maze,x,y,direction,side):
-    realSide = maze[x][y].initWall(direction,side)
-    if realSide == "W" and x>0:
-        maze[x-1][y].wall["E"] = True
-    if realSide == "E" and x<WIDTH:
-        maze[x+1][y].wall["W"] = True
-    if realSide == "S" and y>0:
-        maze[x][y-1].wall["N"] = True
-    if realSide == "N" and y<HEIGHT:
-        maze[x][y+1].wall["S"] = True
-def updateMap(maze):
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            API.setText(x, y, maze[x][y].distance)
-
-        
-def floodFill(maze,goal):
-    q = deque([goal])
-    for x in range(WIDTH):
-            for y in range(HEIGHT):
-                maze[x][y].visited = False
-    maze[goal[0]][goal[1]].visited = True
-    while q:
-        x,y = q.popleft()
-        for direction in DIRECTIONS:
-            if not maze[x][y].wall[direction]:
-                newx = x+DIRECTIONS[direction][0]
-                newy = y+DIRECTIONS[direction][1]
-                if 0<=newx < WIDTH and 0<=newy<HEIGHT and not maze[newx][newy].visited:
-                    maze[newx][newy].distance = maze[x][y].distance+1
-                    log("("+str(newx)+str(newy)+"): "+str(maze[newx][newy].distance))
-                    maze[newx][newy].visited = True
-                    q.append((newx,newy))
-
-
 def main():
-    API.setColor(0, 0, "G")
-    API.setText(0, 0, "abc")
-    currentx = start[0]
-    currenty = start[1]
-    currentDirection = (0, 1)
-    updateMap(maze)
-    while currenty<8:
-        if not API.wallFront():
-            API.moveForward()
-            currentx += currentDirection[0]
-            currenty += currentDirection[1]
-            API.setColor(currentx, currenty, "G")
-        if API.wallRight():
-            initWall(maze,currentx,currenty,currentDirection,"right")
-        if API.wallLeft():
-            initWall(maze,currentx,currenty,currentDirection,"left")
+    mouse = Mouse()
+    global visited
+    i = 0
+    while True:
+        visited[mouse.currentx][mouse.currenty] = True
+        API.setColor(mouse.currentx,mouse.currenty,"G")
         if API.wallFront():
-            initWall(maze,currentx,currenty,currentDirection,"front")
-            floodFill(maze,goal)
-            log(maze[currentx][currenty].distance)
+            updateWall(walls,mouse.currentx,mouse.currenty,mouse.direction,"front")
+
+        if API.wallLeft():
+            updateWall(walls,mouse.currentx,mouse.currenty,mouse.direction,"left")
+
+        if API.wallRight():
+            updateWall(walls,mouse.currentx,mouse.currenty,mouse.direction,"right")
+
+        keep_exploring = NewfloodFill(distances, walls, visited)
+
+        if not keep_exploring:
+            log("--- KHAM PHA HOAN TAT! ---")
+            # Khi khám phá xong, bạn có thể break
+            # Hoặc chuyển sang chế độ "Speed Run" (chạy về ô 0, rồi chạy đến GOAL)
+            break
+        move(mouse,distances,walls)
+        
+        displayDistances(distances)
 
 
 if __name__ == "__main__":
